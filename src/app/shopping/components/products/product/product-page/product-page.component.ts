90faeb8c789a6348fa5de104ca6fe062be912b1d
Unknown <davidk@iastate.edu>
2018-03-24 12:46:58 -0500

    Auto stash before merge of "Product-Page" and "origin/Product-Page"

--- src/app/shopping/components/products/product/product-page/product-page.component.ts
+++ src/app/shopping/components/products/product/product-page/product-page.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'shared/services/product.service';
-import {UserService} from 'shared/services/user.service';
-import {AuthService} from 'shared/services/auth.service';
 import getShoeSizes from 'app/getShoeSizes';
-import {IAppUser} from 'shared/models/app-user';
 
 @Component({
   selector: 'app-product-page',
@@ -13,32 +10,33 @@ import {IAppUser} from 'shared/models/app-user';
 })
 export class ProductPageComponent implements OnInit {
   product: any;
-  appUser: IAppUser = {} as IAppUser;
   isDataAvailable: Boolean;
-  titleDashes;
   shoeSizes: Array<String>;
-    constructor(
+
+  constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
-    private userService: UserService,
-    private auth: AuthService
-  ) {
-  this.isDataAvailable = false;
+  ) { 
+    this.isDataAvailable = false;
   }
 
   async ngOnInit() {
-    this.auth.appUser$.subscribe(user => this.appUser = user);
+    let titleDashes;
     this.route.paramMap.subscribe(params => {
-      this.titleDashes = params.get('title');
+      titleDashes = params.get('title');
     });
-    let title = this.titleDashes.split('-').join(' ');
+    let title = titleDashes.split('-').join(' ');
     this.product = await this.productService.getByTitle(title);
     this.shoeSizes = getShoeSizes();
     this.isDataAvailable = true;
   }
 
-  createSwapRecord() {
-      this.productService.createSwap(this.product.ID, this.appUser.ID, 5, 5);
+  tradeShoe($event) {
+    console.log($event.target.value);
+  }
+  
+  desiredShoe($event) {
+    
   }
 
 }
