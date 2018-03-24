2c3d66f7cdda0ebb4a8c053c4f9e455ad622b22d
mwall <mwall@iastate.edu>
2018-03-23 22:25:43 -0500

    init

---
+++ src/private/firebase-key.ts
@@ -0,0 +1,8 @@
+export const fireBaseKey = {
+  apiKey: 'AIzaSyB08FZ4nd2_1kenBqJzv7t-u0f0q2W0UIo',
+  authDomain: 'size-swap.firebaseapp.com',
+  databaseURL: 'https://size-swap.firebaseio.com',
+  projectId: 'size-swap',
+  storageBucket: 'size-swap.appspot.com',
+  messagingSenderId: '108876455375'
+};
